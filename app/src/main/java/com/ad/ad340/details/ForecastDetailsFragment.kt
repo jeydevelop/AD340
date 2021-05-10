d8485fdbb8155f484f759b9804d597e6adc45708
jeydevelop <jefferykumi@gmail.com>
2021-05-10 11:04:36 -0500

    added view binding to androis block in gradle

--- app/src/main/java/com/ad/ad340/details/ForecastDetailsFragment.kt
+++ app/src/main/java/com/ad/ad340/details/ForecastDetailsFragment.kt
@@ -4,32 +4,39 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.ad.ad340.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val DATE_FORMAT = SimpleDateFormat("MM-dd-yyyy")
 
 class ForecastDetailsFragment : Fragment() {
 
     private val args: ForecastDetailsFragmentArgs by navArgs()
+
     private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val layout = inflater.inflate(R.layout.fragment_forecast_details, container, false)
 
         tempDisplaySettingManager = TempDisplaySettingManager(requireContext())
 
         val tempText = layout.findViewById<TextView>(R.id.tempText3)
         val descriptionText = layout.findViewById<TextView>(R.id.descriptionText3)
+        val dateText = layout.findViewById<TextView>(R.id.dateText)
+        val forecastIcon = layout.findViewById<ImageView>(R.id.forecasticon)
 
         tempText.text = formatTempForDisplay(args.temp, tempDisplaySettingManager.getTempDisplaySetting())
         descriptionText.text = args.description
+
+        dateText.text = DATE_FORMAT.format(Date(args.date * 1000))
+        forecastIcon.
 
 
 
